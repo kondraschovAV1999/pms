7bb7a69caf927ef5bb7bfcc9d96eacec4cb8982b
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2024-11-13 23:10:15 -0500

    initial commit

---
+++ model/src/test/java/org/prison/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prison.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
